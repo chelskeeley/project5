38219adc8870fe94329756a5f600bd4fc8e89fff
Chelsea Keeley <chelsea.keeley@gmail.com>
2017-11-21 19:16:59 -0500

    Adding donuts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Donut from './donut';
+
+const donuts = ['Sour Cream Glazed', 'Honey Dip', 'Boston Cream'];
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          {donuts.map((donut)=>{
+            return (
+              <Donut donutName={donut}/>
+            )
+          })}
         </div>
       )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+//check the movieDB codealong in the react section to learn about routing,to have different views (pages) but just the one index.html
